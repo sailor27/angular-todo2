414dbf80133bdabc66354851f60e525bd485495c
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-01-17 21:20:46 -0800

    add FormsModule to app.module.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
